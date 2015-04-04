ae819f03e7cbfff4428e4e3e676f77ea79d73ed4
Dalton63841 <loudest_silence1@yahoo.com>
2015-04-04 10:45:44 -0500

    Update and rename shutdown.py to default.py

--- default.py
+++ default.py
@@ -12,24 +12,24 @@ user = __addon__.getSetting("username")
 command = __addon__.getSetting("command")
 icon = __addon__.getAddonInfo('icon')
 
-xbmc.executebuiltin('Notification(Server Slayer,Checking Remote Server Status,5000,%s)' % icon)
+xbmc.executebuiltin('Notification(Nas Power,Checking Remote Server Status,5000,%s)' % icon)
 result = commands.getoutput("ping -c1 " + ipadd)
 if result.find("100% packet loss") == -1:
-    xbmc.executebuiltin('Notification(Server Slayer,Shutting Down Remote Server,3000,%s)' % icon)
+    xbmc.executebuiltin('Notification(Nas Power,Shutting Down Remote Server,3000,%s)' % icon)
     os.system('ssh %s@%s %s ' % (user, ipadd, command))
     result = True
     while (result == True):
         check = commands.getoutput("ping -c1 " + ipadd)
         if check.find("100% packet loss") != -1:
 	    result = False
-	    xbmc.executebuiltin('Notification(Server Slayer,Server has successfully powered off,3000,%s)' % icon)
+	    xbmc.executebuiltin('Notification(Nas Power,Server has successfully powered off,3000,%s)' % icon)
 else:
-    xbmc.executebuiltin('Notification(Server Slayer,Waking Remote Server,3000,%s)' % icon)
+    xbmc.executebuiltin('Notification(Nas Power,Waking Remote Server,3000,%s)' % icon)
     xbmc.executebuiltin("WakeOnLan(%s)" % mac)
     result = False
     while (result == False):
         check = commands.getoutput("ping -c1 " + ipadd)
         if check.find("100% packet loss") == -1:
             result = True
-            xbmc.executebuiltin('Notification(Server Slayer,Server has successfully powered on,3000,%s)' % icon)
+            xbmc.executebuiltin('Notification(Nas Power,Server has successfully powered on,3000,%s)' % icon)
 
